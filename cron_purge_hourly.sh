4b96125af64cad3a0c2e7f27c0c27fbaa80f9fd2
Roman Golota <rpgolota@gmail.com>
2021-07-26 20:52:25 +0300

    added script that will be run by cron

---
+++ cron_purge_hourly.sh
@@ -0,0 +1,6 @@
+# place inside folder containing run.sh
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+bash run.sh sync --age=120 --pull=private
+bash run.sh purge --cores=all
+bash run.sh sync --age=120 --push=publishable
